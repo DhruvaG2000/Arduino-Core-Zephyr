f3852b2aafbc2cdbf472e6da91fc82e105ce2220
Dhruva Gole <goledhruva@gmail.com>
2022-06-14 10:43:04 +0530

    feat/I2C: add all function prototypes
    * Blank templates of functions necessary for I2c are ready.
    * The code compiled so far
    * TODO: Start adding actual zephyr implementations
    
    Signed-off-by: Dhruva Gole <goledhruva@gmail.com>

--- libraries/Wire/Wire.cpp
+++ libraries/Wire/Wire.cpp
@@ -1,5 +1,49 @@
 #include "Wire.h"
 
 void arduino::ZephyrI2C::begin() {
-	const struct device *dev = DEVICE_DT_GET_ANY(adi_adxl372);
+  // const struct device *dev = DEVICE_DT_GET_ANY(adi_adxl372);
+  printk("IIC begin\n");
 }
+
+void arduino::ZephyrI2C::begin(uint8_t slaveAddr) {}
+
+void arduino::ZephyrI2C::end() {}
+
+void arduino::ZephyrI2C::setClock(uint32_t freq) {}
+
+void arduino::ZephyrI2C::beginTransmission(uint8_t address) {}
+
+uint8_t arduino::ZephyrI2C::endTransmission(bool stopBit) { return 2; }
+
+uint8_t arduino::ZephyrI2C::endTransmission(void) {
+  return endTransmission(true);
+}
+
+size_t arduino::ZephyrI2C::requestFrom(uint8_t address, size_t len,
+                                       bool stopBit) {
+
+  return len;
+}
+
+size_t arduino::ZephyrI2C::requestFrom(uint8_t address, size_t len) {
+  return requestFrom(address, len, true);
+}
+
+size_t arduino::ZephyrI2C::write(uint8_t data) { return 1; }
+
+size_t arduino::ZephyrI2C::write(const uint8_t *buffer, size_t size) {
+  return size;
+}
+
+int arduino::ZephyrI2C::read() { return 0; }
+
+int arduino::ZephyrI2C::available() { return 1; }
+
+int arduino::ZephyrI2C::peek() { return 1; }
+
+void arduino::ZephyrI2C::flush() {}
+
+void arduino::ZephyrI2C::onReceive(voidFuncPtrParamInt cb) {}
+void arduino::ZephyrI2C::onRequest(voidFuncPtr cb) {}
+
+arduino::ZephyrI2C Wire;
--- libraries/Wire/Wire.h
+++ libraries/Wire/Wire.h
@@ -2,13 +2,43 @@
 
 #include "ArduinoAPI.h"
 #include "HardwareI2C.h"
+#include "Print.h"
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/device.h>
+
+typedef void (*voidFuncPtrParamInt)(int);
 
 namespace arduino {
 
-class ZephyrI2C : public HardwareI2C
-{
-    public:
-        virtual void begin();
+class ZephyrI2C : public HardwareI2C {
+public:
+  virtual void begin();
+  virtual void end();
+  virtual void begin(uint8_t address);
+  virtual void setClock(uint32_t freq);
+
+  virtual void beginTransmission(uint8_t address);
+  virtual uint8_t endTransmission(bool stopBit);
+  virtual uint8_t endTransmission(void);
+
+  virtual size_t requestFrom(uint8_t address, size_t len, bool stopBit);
+  virtual size_t requestFrom(uint8_t address, size_t len);
+
+  virtual void onReceive(void (*)(int));
+  virtual void onRequest(void (*)(void));
+
+  virtual size_t write(uint8_t data);
+  virtual size_t write(int data) { return write((uint8_t)data); };
+  virtual size_t write(const uint8_t *buffer, size_t size);
+  using Print::write;
+  virtual int read();
+  virtual int peek();
+  virtual void flush();
+  virtual int available();
 };
 
-}
+} // namespace arduino
+
+extern arduino::ZephyrI2C Wire;
+
+typedef arduino::ZephyrI2C TwoWire;
