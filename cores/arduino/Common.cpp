b9682e380d22abb5c060d224ce5f7a7ef73e2d65
Dhruva Gole <goledhruva@gmail.com>
2022-06-20 11:17:46 +0530

    feat/gpio: implement digitalRead
    * Also fixes other functions to use legacy non-dt functions
    This way multiple gpios can be configured and used easily.
    
    This commit was successfully built and tested using a button press LED ON example
    
    Signed-off-by: Dhruva Gole <goledhruva@gmail.com>

--- cores/arduino/Common.cpp
+++ cores/arduino/Common.cpp
@@ -9,32 +9,62 @@ long map(long x, long in_min, long in_max, long out_min, long out_max)
 uint16_t makeWord(uint16_t w) { return w; }
 uint16_t makeWord(uint8_t h, uint8_t l) { return (h << 8) | l; }
 
-static struct gpio_dt_spec pin;
-
 void pinMode(pin_size_t pinNumber, PinMode pinMode) {
   if (pinNumber >= 100) {
     pinNumber -= 100;
-    pin = {.port = DEVICE_DT_GET(DT_NODELABEL(gpio1)),
-           .pin = pinNumber,
-           .dt_flags = GPIO_ACTIVE_LOW};
+    if (pinMode == INPUT)
+    {
+    gpio_pin_configure(DEVICE_DT_GET(DT_NODELABEL(gpio1)), pinNumber,
+                       GPIO_INPUT);  
+    }
+    else {
+    gpio_pin_configure(DEVICE_DT_GET(DT_NODELABEL(gpio1)), pinNumber,
+                       GPIO_OUTPUT);
+    }
   } else if (pinNumber < 100) {
-    pin = {.port = DEVICE_DT_GET(DT_NODELABEL(gpio0)),
-           .pin = pinNumber,
-           .dt_flags = GPIO_ACTIVE_LOW};
-  }
-
-  if (pinMode == OUTPUT) {
-    gpio_pin_configure_dt(&pin, GPIO_OUTPUT);
-  } else if (pinMode == INPUT) {
-    gpio_pin_configure_dt(&pin, GPIO_INPUT);
+    if (pinMode == INPUT)
+    {
+      gpio_pin_configure(DEVICE_DT_GET(DT_NODELABEL(gpio0)), pinNumber,
+                       GPIO_INPUT);
+    }
+    else{
+    gpio_pin_configure(DEVICE_DT_GET(DT_NODELABEL(gpio0)), pinNumber,
+                       GPIO_OUTPUT);
+    }
   }
 }
 
 void digitalWrite(pin_size_t pinNumber, PinStatus status) {
-  if (status == HIGH) {
-    gpio_pin_set_dt(&pin, GPIO_ACTIVE_HIGH);
-  } else if (status == LOW) {
-    gpio_pin_set_dt(&pin, GPIO_ACTIVE_LOW);
+  if (pinNumber >= 100) {
+    pinNumber -= 100;
+    if (status == HIGH) {
+      gpio_pin_set(DEVICE_DT_GET(DT_NODELABEL(gpio1)), pinNumber,
+                   GPIO_ACTIVE_HIGH);
+    } else if (status == LOW) {
+      gpio_pin_set(DEVICE_DT_GET(DT_NODELABEL(gpio1)), pinNumber,
+                   GPIO_ACTIVE_LOW);
+    }
+  } else if (pinNumber < 100) {
+    if (status == HIGH) {
+      gpio_pin_set(DEVICE_DT_GET(DT_NODELABEL(gpio0)), pinNumber,
+                   GPIO_ACTIVE_HIGH);
+    } else if (status == LOW) {
+      gpio_pin_set(DEVICE_DT_GET(DT_NODELABEL(gpio0)), pinNumber,
+                   GPIO_ACTIVE_LOW);
+    }
+  }
+}
+
+PinStatus digitalRead(pin_size_t pinNumber) {
+  if (pinNumber >= 100) {
+    pinNumber -= 100;
+    return (gpio_pin_get(DEVICE_DT_GET(DT_NODELABEL(gpio1)), pinNumber) == 1)
+            ? HIGH
+            : LOW;
+  } else if (pinNumber < 100) {
+    return (gpio_pin_get(DEVICE_DT_GET(DT_NODELABEL(gpio0)), pinNumber) == 1)
+            ? HIGH
+            : LOW;
   }
 }
 
