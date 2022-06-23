e0148d29573ab9bd12fc0020498eee6c0980d0a0
Dhruva Gole <goledhruva@gmail.com>
2022-06-23 10:51:44 +0530

    gpio: Add button press led example
    Signed-off-by: Dhruva Gole <goledhruva@gmail.com>

---
+++ examples/button_press_led/src/main.cpp
@@ -0,0 +1,31 @@
+/* Blink inbuilt LED example */
+
+#include <ArduinoAPI.h>
+
+const int buttonPin = D9;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+}
