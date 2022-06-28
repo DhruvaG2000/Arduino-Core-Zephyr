2de961c625ad29c1e3fa5a2b7b479c436cac5ba5
Dhruva Gole <goledhruva@gmail.com>
2022-06-28 22:00:52 +0530

    Removed Arduino API files
    restrucured the entire project to be separate from Arduino Core API code
    
    Signed-off-by: Dhruva Gole <goledhruva@gmail.com>

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 #include "api/ArduinoAPI.h"
-=======
-#include "ArduinoAPI.h"
->>>>>>> f0ea886 (Removed Arduino API files)
 
 /* Zephyr libraries */
 #include "arduino_nano_ble_sense_pinmap.h"
 #include <zephyr/zephyr.h>
-<<<<<<< HEAD
 #include <zephyr/drivers/gpio.h>
-=======
-#include <zephyr/drivers/gpio.h>
->>>>>>> f0ea886 (Removed Arduino API files)
